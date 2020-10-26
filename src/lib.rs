eb0ed9095dca80c58f3c7370f376dd035c1f811d
Juan Aguilar Santillana <mhpoin@gmail.com>
2020-10-26 12:10:34 +0000

    Fix algorithm and refactor to stacked

--- src/lib.rs
+++ src/lib.rs
@@ -1,110 +1,245 @@
-/// Rain for `hours`
-///
-/// Complexity is O(n log n) where n is the number of segments
-///
-/// The complexity is much higher than the typical trapping rain water
-/// which can be solved with 2 pointers.
-///
-/// I was inspired by a bubble sort, but recursion in this case is necessary
-/// or at least I don't know how to stack it because it need a local max height position
-///
-/// Local maximums are sought and the water is distributed in each direction,
-/// stabilizing it in each case.
-///
-/// Every step is documented
-pub fn rain(hours: usize, it: &[usize]) -> Vec<f64> {
-    _rain(hours, (hours * it.len()) as f64, it, false)
-}
+use std::cmp;
 
-fn _rain(hours: usize, liters: f64, it: &[usize], left: bool) -> Vec<f64> {
-    let size = it.len();
-
-    // End
-    if size == 1 {
-        return vec![liters];
-    } else if size == 0 {
-        return vec![];
+/// Trapped rain water
+pub fn rained(it: &[usize]) -> usize {
+    let mut right = Vec::with_capacity(it.len());
+    let mut left = Vec::with_capacity(it.len());
+    for (l, r) in it.iter().copied().zip(it.iter().copied().rev()) {
+        let last_left = left.last().copied().unwrap_or(0);
+        left.push(cmp::max(last_left, l));
+        let last_right = right.last().copied().unwrap_or(0);
+        right.push(cmp::max(last_right, r));
     }
 
+    left.into_iter()
+        .zip(right.into_iter().rev())
+        .zip(it.iter().copied())
+        .map(|((l, r), i)| cmp::min(l, r) - i)
+        .sum()
+}
+
+#[derive(Copy, Clone)]
+enum At {
+    L(usize),
+    R(usize),
+}
+use At::*;
+
+struct Node<'a> {
+    at: At,
+    cur: &'a [usize],
+    liters: f64,
+}
+
+/// Rain for `hours`
+///
+/// Complexity for serial version is O(n log n), O(n^2) worst case, O(n) best case,
+/// where n is the number of segments
+///
+///
+/// Divide and conquer solved with a fork-join task (left or right) // TODO
+///
+/// Merge sort parallel algorithm variant
+///
+/// TODO: Describe algorithm, compute parallel complexity
+///
+/// Linear maps describing are:
+/// (\[N\], +Q) -> +Q, where N natural, +Q positive rational and 0
+///
+/// Every linear map describe the liters for each full or totally empty "waterscape"
+///
+///
+pub fn rain(hours: usize, it: &[usize]) -> Vec<f64> {
+    let hours = hours as f64;
+    match it.len() {
+        0 => return vec![],
+        1 => return vec![hours],
+        _ => (),
+    }
+
+    let mut stack = vec![Node {
+        at: R(0),
+        cur: it,
+        liters: hours * it.len() as f64,
+    }];
+
+    // TODO: implement join by sorted chunks
+    let mut result = vec![0.0; it.len()];
+
+    // TODO: fork-join
+    // Diffusion equation is implemented by divide and conquer over merge sort structure
+    while let Some(n) = stack.pop() {
+        node(hours, n, &mut result, &mut stack);
+    }
+
+    result
+}
+
+fn node<'a>(
+    hours: f64,
+    Node {
+        at,
+        cur,
+        mut liters,
+    }: Node<'a>,
+    result: &mut [f64],
+    stack: &mut Vec<Node<'a>>,
+) {
     // Max height segment
-    let max = it
+    let (max_n, max_v) = cur
         .iter()
         .enumerate()
         .max_by_key(|(_, x)| *x)
-        .map(|(i, x)| (i, *x))
+        .map(|(i, x)| (i + 1, *x))
         .expect("minimum one");
 
+    // Result iterator
+    let r_it = match at {
+        R(n) => &mut result[n..n + cur.len()],
+        L(n) => &mut result[n - cur.len()..n],
+    };
+
     // Available space
-    let total_vec: Vec<usize> = it.iter().map(|x| (max.1 - x)).collect();
+    let t_it = cur.iter().map(|x| (max_v - x) as f64);
 
     // Total available space
-    let total: f64 = total_vec.iter().map(|x| *x as f64).sum();
+    let total: f64 = t_it.clone().sum();
 
     if (total - liters).abs() < f64::EPSILON {
-        // Litters as equal, the topology is the same
-        total_vec.into_iter().map(|x| x as f64).collect()
+        // Litters is equal, the topology is the same
+        for (r, t) in r_it.iter_mut().zip(t_it) {
+            *r = t;
+        }
     } else if total < liters {
-        // Litters as greater, the topology is the same
-        let mut res = Vec::with_capacity(total_vec.len());
+        // Litters is greater, the topology is the same
         let diff = liters - total;
 
         // Calculate portion for each
-        let portion = diff / size as f64;
-
-        // Sum them
-        for total in total_vec {
-            res.push(total as f64 + portion);
+        let portion = diff / cur.len() as f64;
+        for (r, t) in r_it.iter_mut().zip(t_it) {
+            *r = t + portion;
         }
-        res
     } else {
-        // Litters as less, unknown topology
-        // Distributed the litters
+        // Litters is less, unknown topology
 
-        // In half water
-        let m = hours as f64 / 2.0;
+        // Middle left and right
+        let mut l_num = max_n;
+        let r_num = l_num;
+
+        // Middle litters
+        let mut middle = 0.0;
+
+        // Find middle and litters in middle
+        let mut is_max = true;
+        let mut last_max = 0;
+        for (i, (avail, r)) in t_it
+            .clone()
+            .take(l_num)
+            .zip(r_it.iter_mut())
+            .rev()
+            .enumerate()
+        {
+            let diff = hours - avail;
+            if -f64::EPSILON < diff {
+                middle += diff;
+            } else if (diff).abs() > f64::EPSILON {
+                if !is_max {
+                    for i in r_it
+                        .iter_mut()
+                        .take(max_n)
+                        .rev()
+                        .skip(last_max + 1)
+                        .take(i - last_max)
+                    {
+                        liters += *i;
+                        *i = 0.0;
+                    }
+                    middle = r_it
+                        .iter()
+                        .rev()
+                        .take(last_max + 1)
+                        .map(|x| hours - x)
+                        .sum();
+                    l_num = max_n - (last_max + 1);
+                }
+
+                break;
+            }
+
+            is_max = avail < f64::EPSILON;
+            if is_max {
+                last_max = i;
+            } else {
+                liters -= avail;
+                *r = avail;
+            }
+            l_num -= 1;
+        }
+
+        // In middle water
+        let m = middle / 2.0;
 
         // Left water
-        let l_num = max.0;
-        let mut l_liters = l_num as f64 * hours as f64 + m;
-        let l_total: f64 = total_vec[..l_num].iter().map(|x| *x as f64).sum();
-
+        let mut l_liters = l_num as f64 * hours + m;
+        let l_total: f64 = t_it.clone().take(l_num).sum();
         // Right water
-        let r_num = total_vec.len() - (max.0 + 1);
-        let mut r_liters = r_num as f64 * hours as f64 + m;
-        let r_total: f64 = total_vec[l_num + 1..].iter().map(|x| *x as f64).sum();
+        let mut r_liters = (cur.len() - r_num) as f64 * hours + m;
+        let r_total: f64 = t_it.clone().skip(r_num).sum();
 
-        // Difference between real
+        // Propagate surplus over start
         let diff = liters - (l_liters + r_liters);
-        if left {
-            l_liters += diff;
-        } else {
-            r_liters += diff;
-        }
+        let (l_at, r_at) = match at {
+            R(n) => {
+                l_liters += diff;
+                (n + l_num, n + r_num)
+            }
+            L(n) => {
+                r_liters += diff;
+                (n - cur.len() + l_num, n - cur.len() + r_num)
+            }
+        };
 
-        // Move surplus
+        // Rectify surplus to left
         if l_liters > l_total {
-            let d = l_liters - l_total;
-            l_liters -= d;
-            r_liters += d;
-        } else if r_liters > r_total {
-            let d = r_liters - r_total;
-            r_liters -= d;
-            l_liters += d;
+            let diff = l_liters - l_total;
+            r_liters += diff;
+            l_liters -= diff;
         }
 
-        let mut result = Vec::with_capacity(it.len());
+        // Rectify surplus to right
+        if r_liters > r_total {
+            let diff = r_liters - r_total;
+            r_liters -= diff;
+            l_liters += diff;
+        }
 
-        // Distribute left litters
-        result.extend(_rain(hours, l_liters, &it[..l_num], false));
+        // New node left
+        let left = &cur[..l_num];
+        if !left.is_empty() {
+            if left.len() == 1 {
+                r_it[l_num - 1] = l_liters;
+            } else {
+                stack.push(Node {
+                    at: L(l_at),
+                    cur: left,
+                    liters: l_liters,
+                });
+            }
+        }
 
-        // Recursion is necessary since you have to mark the position of the local maximum.
-        // Local max height uncovered by water
-        result.push(0.0);
-
-        // Distribute right litters
-        result.extend(_rain(hours, r_liters, &it[l_num + 1..], true));
-
-        result
+        // New node right
+        let right = &cur[r_num..];
+        if !right.is_empty() {
+            if right.len() == 1 {
+                r_it[r_num] = r_liters;
+            } else {
+                stack.push(Node {
+                    at: R(r_at),
+                    cur: right,
+                    liters: r_liters,
+                });
+            }
+        }
     }
 }
 
@@ -209,5 +344,239 @@ mod test {
         let exp = vec![part, 3.0 + part, 7.0 + part, 0.0, 0.0, 5.5, 6.5, 0.0, 4.5];
         assert_eq!(exp.iter().sum::<f64>(), 3.0 * res.len() as f64);
         assert_eq!(rain(3, &res), exp);
+    }
+
+    #[test]
+    fn failed() {
+        let res = vec![1, 8, 8, 1];
+        assert_eq!(rain(1, &res), vec![2.0, 0.0, 0.0, 2.0]);
+
+        let res = vec![8, 1, 8, 8, 1];
+        assert_eq!(rain(1, &res), vec![0.0, 3.0, 0.0, 0.0, 2.0]);
+
+        let res = vec![5, 8, 7, 7, 8, 1];
+        assert_eq!(rain(1, &res), vec![2.0, 0.0, 1.0, 1.0, 0.0, 2.0]);
+
+        let res = vec![5, 9, 7, 7, 8, 1];
+        assert_eq!(rain(1, &res), vec![1.5, 0.0, 1.0, 1.0, 0.0, 2.5]);
+
+        let res = vec![5, 8, 7, 7, 9, 1];
+        assert_eq!(rain(1, &res), vec![2.5, 0.0, 1.0, 1.0, 0.0, 1.5]);
+    }
+
+    #[test]
+    fn test() {
+        let res = vec![5, 8, 7, 7, 8, 1];
+
+        assert_eq!(rained(&res), 2);
+    }
+
+    #[test]
+    fn possible() {
+        let res = vec![
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        ];
+        let part = 5.0 / 6.0;
+
+        #[rustfmt::skip]
+        let exp = vec![
+            5.0 + part, 4.0 + part, 3.0 + part, 2.0 + part, 1.0 + part, part,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+        ];
+        assert_eq!(5 + 4 + 3 + 2 + 1 + 5 /* 5.0 / 6.0 * 6.0 */, res.len());
+        let r = rain(1, &res);
+        assert_eq!(r, exp);
+    }
+
+    #[test]
+    fn double_possible() {
+        #[rustfmt::skip]
+        let res = vec![
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        ];
+        let part = 3.0 / 4.0;
+        let part2 = 11.0 / 12.0;
+
+        #[rustfmt::skip]
+            let exp = vec![
+            5.0 + part, 4.0 + part, 3.0 + part, 2.0 + part, 1.0 + part, part,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+
+            5.0 + part2, 4.0 + part2, 3.0 + part2, 2.0 + part2, 1.0 + part2, part2,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+        ];
+        assert_eq!(30.0 + (3.0 / 4.0 + 11.0 / 12.0) * 6.0, res.len() as f64);
+        let r = rain(1, &res);
+        assert_eq!(r, exp);
+    }
+
+    #[test]
+    fn left_propagate() {
+        let res = vec![5, 8, 8, 6, 6, 8, 8, 1];
+        let exp = vec![2.0, 0.0, 0.0, 2.0, 2.0, 0.0, 0.0, 2.0];
+        assert_eq!(exp.iter().sum::<f64>(), res.len() as f64);
+        assert_eq!(rain(1, &res), exp);
+
+        let res = vec![5, 8, 8, 8, 6, 6, 8, 1];
+        let exp = vec![2.5, 0.0, 0.0, 0.0, 2.0, 2.0, 0.0, 1.5];
+        assert_eq!(exp.iter().sum::<f64>(), res.len() as f64);
+        assert_eq!(rain(1, &res), exp);
+
+        let res = vec![5, 8, 6, 6, 8, 8, 8, 1];
+        let exp = vec![1.5, 0.0, 2.0, 2.0, 0.0, 0.0, 0.0, 2.5];
+        assert_eq!(exp.iter().sum::<f64>(), res.len() as f64);
+        assert_eq!(rain(1, &res), exp);
+    }
+
+    #[test]
+    fn double_possible_left() {
+        #[rustfmt::skip]
+            let res = vec![
+            6, 5 ,4, 3, 2, 1,
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        ];
+        let part = 11.0 / 20.0;
+        let part2 = 11.0 / 12.0;
+
+        #[rustfmt::skip]
+            let exp = vec![
+            0.0,
+            part, 1.0 + part, 2.0 + part, 3.0 + part, 4.0 + part,
+            4.0 + part, 3.0 + part, 2.0 + part, 1.0 + part, part,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0,
+            5.0 + part2, 4.0 + part2, 3.0 + part2, 2.0 + part2, 1.0 + part2, part2,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+        ];
+        assert_eq!(
+            (4.0 + 3.0 + 2.0 + 1.0) * 3.0 + 5.0 + part * 10.0 + part2 * 6.0,
+            res.len() as f64
+        );
+        let r = rain(1, &res);
+        assert_eq!(r, exp);
+    }
+
+    #[test]
+    fn double_possible_left_2hours() {
+        let res = vec![1, 2, 3, 4, 5, 6];
+
+        let part = 2.0 / 5.0;
+
+        let exp = vec![4.0 + part, 3.0 + part, 2.0 + part, 1.0 + part, part, 0.0];
+        assert_eq!(part * 5.0 + 10.0, (res.len() * 2) as f64);
+        let r = rain(2, &res);
+        assert_eq!(r, exp);
+
+        #[rustfmt::skip]
+        let res = vec![
+            6, 5 ,4, 3, 2, 1,
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        ];
+        let part = (51.0 - (6.0 + 5.0 + 4.0 + 3.0 + 2.0 + 1.0) * 2.0) / 13.0;
+        let part2 = (41.0 - (8.0 + 7.0 + 6.0 + 5.0 + 4.0 + 3.0 + 2.0 + 1.0)) / 9.0;
+
+        #[rustfmt::skip]
+        let exp = vec![
+            1.0 + part,
+            2.0 + part, 3.0 + part, 4.0 + part, 5.0 + part, 6.0 + part,
+            6.0 + part, 5.0 + part, 4.0 + part, 3.0 + part, 2.0 + part,
+            1.0 + part, part,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            8.0 + part2, 7.0 + part2, 6.0 + part2, 5.0 + part2, 4.0 + part2, 3.0 + part2,
+            2.0 + part2, 1.0 + part2, part2,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+        ];
+        assert_eq!(
+            (6.0 + 5.0 + 4.0 + 3.0 + 2.0 + 1.0) * 3.0 + 8.0 + 7.0 + part * 13.0 + part2 * 9.0,
+            res.len() as f64 * 2.0
+        );
+        let r = rain(2, &res);
+        assert_eq!(r, exp);
+    }
+
+    #[test]
+    fn another_left() {
+        let res = vec![1, 2, 3, 4, 5, 6, 7, 7, 7];
+
+        let part = 1.0 / 2.0;
+
+        #[rustfmt::skip]
+            let exp = vec![
+            5.0 + part, 4.0 + part, 3.0 + part, 2.0 + part, 1.0 + part, part,
+            0.0, 0.0, 0.0
+        ];
+        assert_eq!(part * 6.0 + 15.0, (res.len() * 2) as f64);
+        let r = rain(2, &res);
+        assert_eq!(r, exp);
+
+        let res = vec![1, 2, 3, 4, 5, 6, 7, 7, 7, 5];
+
+        let part = 1.0 / 2.0;
+
+        #[rustfmt::skip]
+            let exp = vec![
+            5.0 + part, 4.0 + part, 3.0 + part, 2.0 + part, 1.0 + part, part,
+            0.0, 0.0, 0.0,
+            2.0
+        ];
+        assert_eq!(part * 6.0 + 15.0 + 2.0, (res.len() * 2) as f64);
+        let r = rain(2, &res);
+        assert_eq!(r, exp);
+
+        let res = vec![1, 2, 1, 7, 0, 6, 7, 5];
+
+        let part = 2.0 / 3.0;
+        let part2 = 1.0 / 2.0;
+
+        #[rustfmt::skip]
+            let exp = vec![
+            2.0 + part, 1.0 + part, 2.0 + part,
+            0.0,
+            6.0 + part2, part2,
+            0.0,
+            2.0
+        ];
+        assert_eq!(
+            part * 3.0 + part2 * 2.0 + 5.0 + 6.0 + 2.0,
+            (res.len() * 2) as f64
+        );
+
+        const ERROR: f64 = 1e-15;
+        let r = rain(2, &res);
+        assert!(r
+            .into_iter()
+            .zip(exp.into_iter())
+            .all(|(r, exp)| (r - exp).abs() < ERROR));
+    }
+
+    #[test]
+    fn left_right() {
+        let res = vec![1, 2, 3, 4, 5, 6, 7, 7, 7, 1];
+
+        #[rustfmt::skip]
+            let exp = vec![
+            5.0, 4.0, 3.0, 2.0, 1.0, 0.0,
+            0.0, 0.0, 0.0,
+            5.0
+        ];
+        assert_eq!(exp.iter().sum::<f64>(), (res.len() * 2) as f64);
+        let r = rain(2, &res);
+        assert_eq!(r, exp);
+
+        let res = vec![1, 2, 3, 4, 5, 6, 7, 6, 6, 7, 1];
+
+        #[rustfmt::skip]
+            let exp = vec![
+            5.0, 4.0, 3.0, 2.0, 1.0, 0.0,
+            0.0, 1.0, 1.0, 0.0,
+            5.0
+        ];
+        assert_eq!(exp.iter().sum::<f64>(), (res.len() * 2) as f64);
+        let r = rain(2, &res);
+        assert_eq!(r, exp);
     }
 }
